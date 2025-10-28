d733131fb64af4c48f1c95d33fbcad5aaa3400ef
welin <119814308+WelingtonAguiar1987@users.noreply.github.com>
2025-10-28 19:15:53 -0400

    retirar botão abrir pasta

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ import pytz
 from datetime import timedelta
 import streamlit as st
 import time
+
 try:
     from reportlab.lib.pagesizes import letter
     from reportlab.pdfgen import canvas
     from reportlab.platypus import Table, TableStyle
     from reportlab.lib import colors
 except ImportError as e:
-    st.error("Biblioteca reportlab não está instalada. Instale-a com: `pip install reportlab`")
+    st.error(
+        "Biblioteca reportlab não está instalada. Instale-a com: `pip install reportlab`"
+    )
     raise e
 from io import BytesIO
 import os
@@ -24,63 +27,99 @@ sigla_ativo = "MNQ=F"
 nome_ativo = "NASDAQ 100 FUTUROS"
 
 # CONFIGURAÇÃO DA PÁGINA:
-st.set_page_config(page_title='GERENCIAMENTO DE RISCO', page_icon=':warning:', layout='wide')
+st.set_page_config(
+    page_title="GERENCIAMENTO DE RISCO", page_icon=":warning:", layout="wide"
+)
 
 # Inicializar session_state para armazenar resultados
-if 'resultados' not in st.session_state:
+if "resultados" not in st.session_state:
     st.session_state.resultados = None
 
 # DICIONÁRIO DOS ATIVOS FUTUROS OPERADOS:
 futuros = {
-    'MICRO NASDAQ': {'sigla': 'MNQ=F', 'tick': 0.25, 'valor tick': 0.50},
-    'MINI NASDAQ': {'sigla': 'NQ=F', 'tick': 0.25, 'valor tick': 5.00},
-    'MICRO S&P500': {'sigla': 'MES=F', 'tick': 0.25, 'valor tick': 1.25},
-    'MINI S&P500': {'sigla': 'ES=F', 'tick': 0.25, 'valor tick': 12.50},
-    'MICRO DOW JONES': {'sigla': 'MYM=F', 'tick': 1.00, 'valor tick': 0.50},
-    'MINI DOW JONES': {'sigla': 'YM=F', 'tick': 1.00, 'valor tick': 5.00},   
-    'MICRO RUSSELL2000': {'sigla': 'M2K=F', 'tick': 0.10, 'valor tick': 0.50},
-    'MINI RUSSELL2000': {'sigla': 'RTY=F', 'tick': 0.10, 'valor tick': 5.00},  
-    'MICRO OURO': {'sigla': 'MGC=F', 'tick': 0.10, 'valor tick': 1.00},
-    'MINI OURO': {'sigla': 'GC=F', 'tick': 0.10, 'valor tick': 10.00},
-    'MICRO PETRÓLEO WTI': {'sigla': 'MCL=F', 'tick': 0.01, 'valor tick': 100.00}
+    "MICRO NASDAQ": {"sigla": "MNQ=F", "tick": 0.25, "valor tick": 0.50},
+    "MINI NASDAQ": {"sigla": "NQ=F", "tick": 0.25, "valor tick": 5.00},
+    "MICRO S&P500": {"sigla": "MES=F", "tick": 0.25, "valor tick": 1.25},
+    "MINI S&P500": {"sigla": "ES=F", "tick": 0.25, "valor tick": 12.50},
+    "MICRO DOW JONES": {"sigla": "MYM=F", "tick": 1.00, "valor tick": 0.50},
+    "MINI DOW JONES": {"sigla": "YM=F", "tick": 1.00, "valor tick": 5.00},
+    "MICRO RUSSELL2000": {"sigla": "M2K=F", "tick": 0.10, "valor tick": 0.50},
+    "MINI RUSSELL2000": {"sigla": "RTY=F", "tick": 0.10, "valor tick": 5.00},
+    "MICRO OURO": {"sigla": "MGC=F", "tick": 0.10, "valor tick": 1.00},
+    "MINI OURO": {"sigla": "GC=F", "tick": 0.10, "valor tick": 10.00},
+    "MICRO PETRÓLEO WTI": {"sigla": "MCL=F", "tick": 0.01, "valor tick": 100.00},
 }
 
 # Criação e configuração das variáveis
-st.markdown("<h1 style='text-align: center; color: white;'>GERENCIAMENTO DE RISCO ⚠️</h1>", unsafe_allow_html=True)
+st.markdown(
+    "<h1 style='text-align: center; color: white;'>GERENCIAMENTO DE RISCO ⚠️</h1>",
+    unsafe_allow_html=True,
+)
 ativo = st.selectbox("Ativo Operado:", list(futuros.keys()))
 tipo_operacao = st.selectbox("Posição Aberta:", ["COMPRA", "VENDA"])
-preco_abertura_posicao = float(st.number_input("Preço Abertura de Posição:", format="%.2f"))
+preco_abertura_posicao = float(
+    st.number_input("Preço Abertura de Posição:", format="%.2f")
+)
 preco_stop = float(st.number_input("Preço do Stop:", format="%.2f"))
 preco_alvo = float(st.number_input("Preço do Alvo:", format="%.2f"))
 
 # CONDICIONAL PARA SELECIONAR CONTRATOS, BARRIS OU ONÇAS TROY:
-if ativo == 'MICRO NASDAQ':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=85))
-elif ativo == 'MINI NASDAQ':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=17))
-elif ativo == 'MICRO S&P500':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=85)) 
-elif ativo == 'MINI S&P500':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=17))
-elif ativo == 'MICRO DOW JONES':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=17)) 
-elif ativo == 'MINI DOW JONES':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=17))   
-elif ativo == 'MICRO RUSSELL2000':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=17))
-elif ativo == 'MINI RUSSELL2000':
-    total_contratos = int(st.number_input("Quantidade Contratos:", min_value=0, max_value=17))   
-elif ativo == 'MICRO OURO':
-    total_contratos = float(st.number_input("Quantidade Fracionada da Onças Troy:", min_value=0.01, max_value=1.000))
-elif ativo == 'MINI OURO':
-    total_contratos = float(st.number_input("Quantidade Fracionada da Onças Troy:", min_value=0.01, max_value=1.000))
-elif ativo == 'MICRO PETRÓLEO WTI':
-    total_contratos = float(st.number_input("Quantidade Fracionada do Barril:", min_value=0.01, max_value=1.000))    
-         
+if ativo == "MICRO NASDAQ":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=85)
+    )
+elif ativo == "MINI NASDAQ":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=17)
+    )
+elif ativo == "MICRO S&P500":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=85)
+    )
+elif ativo == "MINI S&P500":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=17)
+    )
+elif ativo == "MICRO DOW JONES":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=17)
+    )
+elif ativo == "MINI DOW JONES":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=17)
+    )
+elif ativo == "MICRO RUSSELL2000":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=17)
+    )
+elif ativo == "MINI RUSSELL2000":
+    total_contratos = int(
+        st.number_input("Quantidade Contratos:", min_value=0, max_value=17)
+    )
+elif ativo == "MICRO OURO":
+    total_contratos = float(
+        st.number_input(
+            "Quantidade Fracionada da Onças Troy:", min_value=0.01, max_value=1.000
+        )
+    )
+elif ativo == "MINI OURO":
+    total_contratos = float(
+        st.number_input(
+            "Quantidade Fracionada da Onças Troy:", min_value=0.01, max_value=1.000
+        )
+    )
+elif ativo == "MICRO PETRÓLEO WTI":
+    total_contratos = float(
+        st.number_input(
+            "Quantidade Fracionada do Barril:", min_value=0.01, max_value=1.000
+        )
+    )
+
 if total_contratos == 0:
-    st.error('SELECIONE UMA QUANTIDADE DE CONTRATOS PARA GERAR O CÁLCULO!')
+    st.error("SELECIONE UMA QUANTIDADE DE CONTRATOS PARA GERAR O CÁLCULO!")
 
 capital_total = float(st.number_input("Capital Total da Conta:", format="%.2f"))
+
 
 # Função para calcular e exibir os resultados
 def calculate():
@@ -88,16 +127,16 @@ def calculate():
     ponto_tick = 4  # Ajuste conforme necessário
 
     # Calcula os alvos e stops
-    if tipo_operacao == 'COMPRA':
+    if tipo_operacao == "COMPRA":
         alvo = preco_alvo - preco_abertura_posicao
         stop = preco_abertura_posicao - preco_stop
-    elif tipo_operacao == 'VENDA':
+    elif tipo_operacao == "VENDA":
         alvo = preco_abertura_posicao - preco_alvo
         stop = preco_stop - preco_abertura_posicao
 
     # Calcula o lucro e a perda
-    lucro = ((ativo_detalhes['valor tick'] * ponto_tick) * abs(alvo)) * total_contratos
-    perda = ((ativo_detalhes['valor tick'] * ponto_tick) * abs(stop)) * total_contratos
+    lucro = ((ativo_detalhes["valor tick"] * ponto_tick) * abs(alvo)) * total_contratos
+    perda = ((ativo_detalhes["valor tick"] * ponto_tick) * abs(stop)) * total_contratos
 
     # Calcula a variação de lucro e perda com base no capital total
     var_lucro = (lucro / capital_total) * 100
@@ -110,29 +149,34 @@ def calculate():
         payoff = "indefinido"
 
     # Exibe os resultados
-    st.write(f'Nesta {tipo_operacao.upper()} o seu ALVO é de {abs(alvo):.2f} pontos, com LUCRO de US$ {lucro:.2f}.')
-    st.write(f'Nesta {tipo_operacao.upper()} o seu STOP é de {abs(stop):.2f} pontos, com PERDA de US$ {perda:.2f}.')
-    st.write(f'O PAYOFF desta operação é {payoff:.2f}.')
-    st.write(f'A variação de LUCRO em relação ao capital total é de {var_lucro:.2f}%.')
-    st.write(f'A variação de PERDA em relação ao capital total é de {var_perda:.2f}%.')
-    
+    st.write(
+        f"Nesta {tipo_operacao.upper()} o seu ALVO é de {abs(alvo):.2f} pontos, com LUCRO de US$ {lucro:.2f}."
+    )
+    st.write(
+        f"Nesta {tipo_operacao.upper()} o seu STOP é de {abs(stop):.2f} pontos, com PERDA de US$ {perda:.2f}."
+    )
+    st.write(f"O PAYOFF desta operação é {payoff:.2f}.")
+    st.write(f"A variação de LUCRO em relação ao capital total é de {var_lucro:.2f}%.")
+    st.write(f"A variação de PERDA em relação ao capital total é de {var_perda:.2f}%.")
+
     # Armazena os resultados no session_state
     st.session_state.resultados = {
-        'ativo': ativo,
-        'tipo_operacao': tipo_operacao,
-        'preco_abertura': preco_abertura_posicao,
-        'preco_alvo': preco_alvo,
-        'preco_stop': preco_stop,
-        'total_contratos': total_contratos,
-        'capital_total': capital_total,
-        'alvo_pontos': abs(alvo),
-        'stop_pontos': abs(stop),
-        'lucro': lucro,
-        'perda': perda,
-        'payoff': payoff,
-        'var_lucro': var_lucro,
-        'var_perda': var_perda
+        "ativo": ativo,
+        "tipo_operacao": tipo_operacao,
+        "preco_abertura": preco_abertura_posicao,
+        "preco_alvo": preco_alvo,
+        "preco_stop": preco_stop,
+        "total_contratos": total_contratos,
+        "capital_total": capital_total,
+        "alvo_pontos": abs(alvo),
+        "stop_pontos": abs(stop),
+        "lucro": lucro,
+        "perda": perda,
+        "payoff": payoff,
+        "var_lucro": var_lucro,
+        "var_perda": var_perda,
     }
+
 
 # Função para gerar o PDF
 def generate_pdf(resultados):
@@ -144,19 +188,21 @@ def generate_pdf(resultados):
         # Título
         c.setFont("Helvetica-Bold", 16)
         c.setFillColor(colors.darkblue)
-        c.drawCentredString(width / 2, height - 50, "Relatório de Gerenciamento de Risco")
+        c.drawCentredString(
+            width / 2, height - 50, "Relatório de Gerenciamento de Risco"
+        )
         c.setFont("Helvetica", 12)
         c.setFillColor(colors.black)
         # Usar o fuso horário de Nova York
-        ny_timezone = pytz.timezone('America/New_York')
-        ny_datetime = datetime.datetime.now(ny_timezone).strftime('%d/%m/%Y %H:%M:%S')
+        ny_timezone = pytz.timezone("America/New_York")
+        ny_datetime = datetime.datetime.now(ny_timezone).strftime("%d/%m/%Y %H:%M:%S")
         c.drawCentredString(width / 2, height - 70, f"Data: {ny_datetime}")
 
         # Dados para a tabela
         data = [
             ["Campo", "Valor"],
-            ["Ativo Operado", resultados['ativo']],
-            ["Tipo de Operação", resultados['tipo_operacao']],
+            ["Ativo Operado", resultados["ativo"]],
+            ["Tipo de Operação", resultados["tipo_operacao"]],
             ["Preço de Abertura", f"US$ {resultados['preco_abertura']:.2f}"],
             ["Preço do Alvo", f"US$ {resultados['preco_alvo']:.2f}"],
             ["Preço do Stop", f"US$ {resultados['preco_stop']:.2f}"],
@@ -168,29 +214,33 @@ def generate_pdf(resultados):
             ["Perda", f"US$ {resultados['perda']:.2f}"],
             ["Payoff", f"{resultados['payoff']:.2f}"],
             ["Variação de Lucro", f"{resultados['var_lucro']:.2f}%"],
-            ["Variação de Perda", f"{resultados['var_perda']:.2f}%"]
+            ["Variação de Perda", f"{resultados['var_perda']:.2f}%"],
         ]
 
         # Criar tabela
         table = Table(data)
-        table.setStyle(TableStyle([
-            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('FONTSIZE', (0, 0), (-1, 0), 12),
-            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-            ('BACKGROUND', (0, 1), (-1, -1), colors.white),
-            ('TEXTCOLOR', (0, 1), (-1, -1), colors.black),
-            ('FONTNAME', (0, 1), (-1, -1), 'Helvetica'),
-            ('FONTSIZE', (0, 1), (-1, -1), 10),
-            ('GRID', (0, 0), (-1, -1), 1, colors.black),
-            # Cores para destacar lucro e perda
-            ('TEXTCOLOR', (1, 10), (1, 10), colors.green),  # Lucro
-            ('TEXTCOLOR', (1, 11), (1, 11), colors.red),    # Perda
-            ('TEXTCOLOR', (1, 13), (1, 13), colors.green),  # Variação de Lucro
-            ('TEXTCOLOR', (1, 14), (1, 14), colors.red),    # Variação de Perda
-        ]))
+        table.setStyle(
+            TableStyle(
+                [
+                    ("BACKGROUND", (0, 0), (-1, 0), colors.grey),
+                    ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
+                    ("ALIGN", (0, 0), (-1, -1), "CENTER"),
+                    ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),
+                    ("FONTSIZE", (0, 0), (-1, 0), 12),
+                    ("BOTTOMPADDING", (0, 0), (-1, 0), 12),
+                    ("BACKGROUND", (0, 1), (-1, -1), colors.white),
+                    ("TEXTCOLOR", (0, 1), (-1, -1), colors.black),
+                    ("FONTNAME", (0, 1), (-1, -1), "Helvetica"),
+                    ("FONTSIZE", (0, 1), (-1, -1), 10),
+                    ("GRID", (0, 0), (-1, -1), 1, colors.black),
+                    # Cores para destacar lucro e perda
+                    ("TEXTCOLOR", (1, 10), (1, 10), colors.green),  # Lucro
+                    ("TEXTCOLOR", (1, 11), (1, 11), colors.red),  # Perda
+                    ("TEXTCOLOR", (1, 13), (1, 13), colors.green),  # Variação de Lucro
+                    ("TEXTCOLOR", (1, 14), (1, 14), colors.red),  # Variação de Perda
+                ]
+            )
+        )
 
         # Ajustar largura das colunas
         table_width = width - 100
@@ -204,22 +254,23 @@ def generate_pdf(resultados):
         c.showPage()
         c.save()
         buffer.seek(0)
-        
+
         # Salvar o PDF localmente
         output_dir = "relatorios_gerenciamento_risco"
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-        
+
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         pdf_filename = f"{output_dir}/relatorio_gerenciamento_risco_{timestamp}.pdf"
         with open(pdf_filename, "wb") as f:
             f.write(buffer.read())
-        
+
         buffer.seek(0)
         return buffer, pdf_filename, output_dir
     except Exception as e:
         st.error(f"Erro ao gerar o PDF: {str(e)}")
         return None, None, None
+
 
 # Função para abrir a pasta onde o PDF foi salvo
 def open_folder(folder_path):
@@ -234,10 +285,13 @@ def open_folder(folder_path):
     except Exception as e:
         st.error(f"Erro ao abrir a pasta: {str(e)}")
 
+
 # Botão para calcular
-if st.button('Calcular'):
+if st.button("Calcular"):
     if total_contratos == 0:
-        st.error("Por favor, selecione uma quantidade de contratos válida antes de calcular.")
+        st.error(
+            "Por favor, selecione uma quantidade de contratos válida antes de calcular."
+        )
     else:
         try:
             calculate()
@@ -247,7 +301,7 @@ if st.button('Calcular'):
 
 # Botão para gerar PDF e abrir pasta
 if st.session_state.resultados is not None:
-    if st.button('Gerar PDF'):
+    if st.button("Gerar PDF"):
         resultados = st.session_state.resultados
         pdf_buffer, pdf_filename, output_dir = generate_pdf(resultados)
         if pdf_buffer and pdf_filename and output_dir:
@@ -255,12 +309,10 @@ if st.session_state.resultados is not None:
                 label="Baixar Relatório em PDF",
                 data=pdf_buffer,
                 file_name=os.path.basename(pdf_filename),
-                mime="application/pdf"
+                mime="application/pdf",
             )
             st.success(f"PDF gerado e salvo em: {pdf_filename}")
-            
-            # Botão para abrir a pasta
-            if st.button('Abrir Pasta do Arquivo'):
-                open_folder(output_dir)
+
+
 else:
     st.warning("Realize o cálculo antes de gerar o PDF.")
